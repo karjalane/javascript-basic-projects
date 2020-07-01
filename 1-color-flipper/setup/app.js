c0ed5f0863ee679f3a2542c73233a10a7f83dd55
karjalane <nino.karjalainen@gmail.com>
2020-07-01 18:13:03 +0300

    Color flipper

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "aquamarine", "rosybrown"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+  // Get random number between 0-3 
+  const randomNum = getRandomNum()
+  console.log(randomNum)
+
+  document.body.style.backgroundColor = colors[randomNum]
+  color.textContent = colors[randomNum]
+  console.log(color)
+})
+
+function getRandomNum() {
+  return Math.floor(Math.random() * colors.length)
+}
