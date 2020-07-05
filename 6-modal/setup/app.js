b35d9b7c81ff522a7b6369b205d06ce85c2c2614
karjalane <nino.karjalainen@gmail.com>
2020-07-05 17:25:39 +0300

    Modal and Questions

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -2,3 +2,20 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector('.modal-btn')
+const overlay = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn')
+
+modalBtn.addEventListener('click', () => {
+  console.log(overlay.classList)
+  overlay.classList.toggle('open-modal')
+})
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('open-modal')
+})
+
+closeBtn.addEventListener('click', () => {
+  overlay.classList.remove('open-modal')
+})
--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,28 @@
-//using selectors inside the element
+// using selectors inside the element
+const questions = document.querySelectorAll('.question')
+
+questions.forEach((q) => {
+  // console.log(q)
+  const btn = q.querySelector('.question-btn')
+  // console.log(btn)
+  btn.addEventListener('click', () => {
+    questions.forEach((i) => {
+      if (i !== q) {
+        i.classList.remove('show-text')
+      }
+    })
+
+    q.classList.toggle('show-text')
+  })
+})
+
 // traversing the dom
+// const btns = document.querySelectorAll('.question-btn')
+
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     console.log(e.currentTarget.parentElement.parentElement)
+//     const question = e.currentTarget.parentElement.parentElement
+//     question.classList.toggle('show-text')
+//   })
+// })
